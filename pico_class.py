4d2f24e33868508446ac259bdee4974f4b2c03a9
Mike <myke.dmytrenko@gmail.com>
2021-06-07 16:16:36 +0200

    latest

--- pico_class.py
+++ pico_class.py
@@ -146,7 +146,7 @@ class PicovoiceDemo(Thread):
 if __name__=="__main__":
     recognizer=PicovoiceDemo(
                     keyword_path='/home/mike/catkin_ws/src/recognition/porcupine/resources/keyword_files/linux/alexa_linux.ppn',
-                    context_path='/home/mike/Picovoice/street_en_linux_2021-05-19-utc_v1_6_0.rhn',
+                    context_path='/home/mike/Picovoice/SLAM_en_linux_2021-06-19-utc_v1_6_0.rhn',
                     # porcupine_library_path=args.porcupine_library_path,
                     # porcupine_model_path=args.porcupine_model_path,
                     # porcupine_sensitivity=args.porcupine_sensitivity,
