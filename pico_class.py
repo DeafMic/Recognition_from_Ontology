a4de35ee8cf7e6aeff016993c854d9b7e7ef41ed
Mike <myke.dmytrenko@gmail.com>
2021-11-16 16:52:18 +0100

    timestamps fixed

--- pico_class.py
+++ pico_class.py
@@ -145,7 +145,7 @@ class PicovoiceDemo(Thread):
 
 if __name__=="__main__":
     recognizer=PicovoiceDemo(
-                    keyword_path='/home/mike/catkin_ws/src/recognition/porcupine/resources/keyword_files/linux/alexa_linux.ppn',
+                    keyword_path='/home/mike/catkin_ws/src/recognition/porcupine/resources/keyword_files/linux/hey siri_linux.ppn',
                     context_path='/home/mike/Picovoice/SLAM_en_linux_2021-12-03-utc_v1_6_0.rhn',
                     # porcupine_library_path=args.porcupine_library_path,
                     # porcupine_model_path=args.porcupine_model_path,
