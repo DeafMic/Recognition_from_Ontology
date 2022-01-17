225bb91bc765df409367e1a0c92315e7793704d2
Mike <myke.dmytrenko@gmail.com>
2022-01-17 12:18:15 +0100

    data for parsing added

--- parse_bag.py
+++ parse_bag.py
@@ -7,6 +7,7 @@ import keyboard
 import os
 import sys
 from curtsies import Input
+from datetime import datetime
 os.chdir(sys.path[0])
 pub = rospy.Publisher('index', std.Int16, queue_size=10)
 time_start=0
@@ -16,6 +17,14 @@ ind_arr=[]
 times_arr=[5.1, 15, 36, 55.3, 112.7, 152, 162, 
     221, 232, 288.4, 353, 364.7, 416, 427, 479, 500, 
     549.7, 562.1, 577, 628, 649.8, 669.9, 717, 734.17, 784.5, 805, 820]
+run_once=0
+
+now = datetime.now()
+
+dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
+filename=rospy.get_param("/current_filename")
+fullpath="/home/mike/Output/Corrected/"+filename+"/maps"
+os.makedirs(fullpath, exist_ok=True)
 
 print("FIlenumber is ", sys.argv[1])
 # %%
@@ -26,12 +35,13 @@ def callback(time):
     global ind_arr
     global times_arr
     global time_ind
+    global run_once
     if loopnum==0:
 
         time_start=time
         # print("start time "+str(time))
     
-    loopnum+=1
+    loopnum =1
     # if keyboard.read_key()=='space':
     #    inp=input('Enter needed index as Integer. \n')
        
@@ -42,6 +52,11 @@ def callback(time):
         pub.publish(ind_arr[time_ind])
         time_ind=time_ind+1
         # print("index now" + str(time_ind))
+
+    if time_now>=820 and run_once==0:
+        os.system("rosrun map_server map_saver -f "+ fullpath +"/"+dt_string +  " map:=/map")
+        # print("rosrun map_server map_saver -f "+ fullpath +"/"+dt_string +  " map:=/map")
+        run_once=1
 
     
 
